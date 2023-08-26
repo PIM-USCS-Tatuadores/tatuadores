95495ba09fc7676617cf2d0118ebd2b716bbc30e
Caio Ferrarezi <caioferrarezi2@gmail.com>
2023-08-26 19:53:59 -0300

    adiciona mais testes

---
+++ backend/src/domain/email.test.ts
@@ -0,0 +1,12 @@
+import { Email } from "./email"
+
+describe('Email', () => {
+  test('Deve criar um value object com email', () => {
+    const email = new Email('john@doe.com')
+    expect(email.value).toBe('john@doe.com')
+  })
+
+  test('Deve lançar uma exceção para um email inválido', () => {
+    expect(() => new Email('johndoe.com')).toThrow(new Error('Invalid email'))
+  })
+})
